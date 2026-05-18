0ffa4d725358611b1f248f67ddc4db281316e439
Vova <darkcosmo543@gmail.com>
2026-05-18 16:44:28 +0300

    Add auto-migration on startup

--- InfiniteCaptcha/InfiniteCaptcha.Api/Program.cs
+++ InfiniteCaptcha/InfiniteCaptcha.Api/Program.cs
@@ -32,4 +32,10 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.Run();
